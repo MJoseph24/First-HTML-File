92b0bc5f7f820840ce65b0297879e6f2bee45eaa
Melvin <melvin.joejoseph@gmail.com>
2023-01-06 14:31:36 -0500

    added style changes

--- melvin-react-api-project/src/App.js
+++ melvin-react-api-project/src/App.js
@@ -1,76 +1,61 @@
-import React, { useEffect, useState } from "react";
-import "./App.css";
-import CurrencyRow from "./CurrencyRow";
 
-const BASE_URL = "https://api.exchangeratesapi.io/latest";
+import React, { Component } from 'react';
+import './App.css';
+import Spotify from 'spotify-web-api-js';
+import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import { Container, Row, Col } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody,
+  CardTitle, CardSubtitle, CardImgOverlay } from 'reactstrap';
 
-function App() {
-  const [currencyOptions, setCurrencyOptions] = useState([]);
-  const [fromCurrency, setFromCurrency] = useState();
-  const [toCurrency, setToCurrency] = useState();
-  const [exchangeRate, setExchangeRate] = useState();
-  const [amount, setAmount] = useState(1);
-  const [amountInFromCurrency, setAmountInFromCurrency] = useState(true);
+const spotifyWebApi = new Spotify();
 
-  let toAmount, fromAmount;
-  if (amountInFromCurrency) {
-    fromAmount = amount;
-    toAmount = amount * exchangeRate;
-  } else {
-    toAmount = amount;
-    fromAmount = amount / exchangeRate;
-  }
-
-  useEffect(() => {
-    fetch(BASE_URL)
-      .then((res) => res.json())
-      .then((data) => {
-        const firstCurrency = Object.keys(data.rates)[0];
-        setCurrencyOptions([data.base, ...Object.keys(data.rates)]);
-        setFromCurrency(data.base);
-        setToCurrency(firstCurrency);
-        setExchangeRate(data.rates[firstCurrency]);
-      });
-  }, []);
-
-  useEffect(() => {
-    if (fromCurrency !== undefined && toCurrency !== undefined) {
-      fetch(`${BASE_URL}?base=${fromCurrency}&symbols=${toCurrency}`)
-        .then((res) => res.json())
-        .then((data) => setExchangeRate(data.rates[toCurrency]));
-    }
-  }, [fromCurrency, toCurrency]);
-
-  function handleFromAmountChange(e) {
-    setAmount(e.target.value);
-    setAmountInFromCurrency(true);
-  }
-
-  function handleToAmountChange(e) {
-    setAmount(e.target.value);
-    setAmountInFromCurrency(false);
-  }
-
-  return (
-    <>
-      <h1>Convert</h1>
-      <CurrencyRow
-        currencyOptions={currencyOptions}
-        selectedCurrency={fromCurrency}
-        onChangeCurrency={(e) => setFromCurrency(e.target.value)}
-        onChangeAmount={handleFromAmountChange}
-        amount={fromAmount}
-      />
-      <div className="equals">=</div>
-      <CurrencyRow
-        currencyOptions={currencyOptions}
-        selectedCurrency={toCurrency}
-        onChangeCurrency={(e) => setToCurrency(e.target.value)}
-        onChangeAmount={handleToAmountChange}
-        amount={toAmount}
-      />
-    </>
-  );
-}
-
-export default App;
+class App extends Component {
+  constructor(){
+    super();
+    const params = this.getHashParams();
+    this.state = {
+      loggedIn: params.access_token ? true : false,
+      nowPlaying: { name: 'Not Checked', albumArt: '' },
+      artist: '',
+      album: '',
+      track: '',
+      artistData: [],
+      albumData: [],
+      trackData: [],
+      artistId: '',
+      albumId: '',
+      trackId: '',
+      artistImage: '',
+      albumImage: '',
+      trackImage: '',
+      artistName: '',
+      albumName: '',
+      trackName: '',
+      artistGenre: '',
+      albumGenre: '',
+      trackGenre: '',
+      artistPopularity: '',
+      albumPopularity: '',
+      trackPopularity: '',
+      artistFollowers: '',
+      albumFollowers: '',
+      trackFollowers: '',
+      artistUrl: '',
+      albumUrl: '',
+      trackUrl: '',
+      artistAlbums: [],
+      albumTracks: [],
+      trackAlbum: '',
+      trackArtist: '',
+      trackDuration: '',
+      trackExplicit: '',
+      trackPopularity: '',
+      trackPreview: '',
+      trackUrl: '',
+      trackAlbumImage: '',
+      trackArtistImage: '',
+      trackArtistName: '',
+      trackAlbumName: '',
+      trackArtistGenre: '',
+      trackArtistPopularity: '',
+      trackArtistFollowers: ''
